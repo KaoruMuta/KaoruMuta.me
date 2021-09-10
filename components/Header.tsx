29b3eedc13e5e5d22d017223603b78ecdf495b93
muta <muttaroni8827@gmail.com>
2021-09-10 22:26:06 +0900

    style: arrange header UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,12 @@
 const Header = () => {
-  return <header>KaoruMuta.me.</header>;
+  return (
+    <header className='flex flex-row font-sans font-semibold mt-8'>
+      <span className='flex-1'>KaoruMuta.me.</span>
+      <span className='flex-none'>Career</span>
+      <span className='flex-none ml-8'>Works</span>
+      <span className='flex-none ml-8'>Articles</span>
+    </header>
+  );
 };
 
 export default Header;
