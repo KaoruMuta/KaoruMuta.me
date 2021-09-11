38bce71efd8973a7c1fd4ed5724d63e85375d246
muta <muttaroni8827@gmail.com>
2021-09-11 12:51:44 +0900

    feat: attach icon in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
+import { FcApproval } from 'react-icons/fc';
+
 const Header = () => {
   return (
     <header className='flex flex-row font-sans font-semibold mt-8'>
-      <span className='flex-1'>KaoruMuta.me.</span>
+      <span className='inline-flex flex-1 items-center'>
+        KaoruMuta.me.
+        <FcApproval className='ml-1' />
+      </span>
       <span className='flex-none'>Career</span>
       <span className='flex-none ml-8'>Works</span>
       <span className='flex-none ml-8'>Articles</span>
