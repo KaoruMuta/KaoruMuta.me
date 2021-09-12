9f32d840325cc97d7a2effa6b6eb0572f5baa18b
muta <muttaroni8827@gmail.com>
2021-09-12 17:37:36 +0900

    feat: add border styling change by current path

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,21 @@
 import { FcApproval } from 'react-icons/fc';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <header className='flex flex-row font-sans font-semibold mt-8 space-x-4 xl:space-x-8'>
+    <header className='flex flex-row items-start font-sans font-semibold mt-8 space-x-4 xl:space-x-8'>
       <span className='inline-flex flex-1 items-center cursor-default'>
         KaoruMuta.me.
         <FcApproval className='ml-1' />
       </span>
       <Link href='/'>
-        <a>Home</a>
+        <a className={router.asPath === '/' ? 'border-b border-black' : 'border-b border-white'}>Home</a>
       </Link>
       <Link href='/articles'>
-        <a>Articles</a>
+        <a className={router.asPath === '/articles' ? 'border-b border-black' : 'border-b border-white'}>Articles</a>
       </Link>
     </header>
   );
