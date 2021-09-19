de77af0b572c0d5acb6a34a9521b6c9f7f96b4d6
muta <muttaroni8827@gmail.com>
2021-09-19 12:16:04 +0900

    feat: create blog page as a mock

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,15 @@
 import { NextPage } from 'next';
 
 const Blog: NextPage = () => {
-  return <div>tionblog</div>;
+  return (
+    <section className='font-sans divide-y mt-8 xl:mt-16'>
+      <h1 className='font-semibold text-2xl xl:text-4xl'>tionblog</h1>
+      <section className='flex flex-row mt-2 pt-2'>
+        <time>2021/09/01</time>
+        <h2 className='ml-16 font-semibold'>Next.jsでブログを作成しました</h2>
+      </section>
+    </section>
+  );
 };
 
 export default Blog;
