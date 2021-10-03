30ac96efdd22a1c926d9691a7a5487585167ddd9
muta <muttaroni8827@gmail.com>
2021-10-03 11:41:05 +0900

    refactor: replace hardcoded string with AppConstants variable

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -10,17 +10,10 @@ const Privacy = () => {
       </Head>
       <article className="prose max-w-none my-16">
         <Title name={AppConstants.PrivacyPage.NAME} />
-        <h2>個人情報の利用目的</h2>
-        <p>
-          当サイトでは、お問い合わせの際、名前やメールアドレス等の個人情報を取得することがあります。
-          取得した個人情報は、お問い合わせに対する回答や必要な情報を電子メールなどをでご連絡する場合に利用させていただくものであり、これらの目的以外では利用いたしません。
-        </p>
-        <h2>広告について</h2>
-        <p>
-          当ブログでは、第三者配信の広告サービスであるGoogle
-          Adsenseを利用しており、ユーザーの興味に応じた商品やサービスの広告を表示するため、Cookieを使用しております。
-          Cookieを使用することで当サイトはお客様のコンピュータを識別できるようになりますが、お客様個人を特定できるものではありません。
-        </p>
+        <h2>{AppConstants.PrivacyPage.CONTENTS.PERSONAL_INFORMATION.title}</h2>
+        <p>{AppConstants.PrivacyPage.CONTENTS.PERSONAL_INFORMATION.description}</p>
+        <h2>{AppConstants.PrivacyPage.CONTENTS.ADVERTISEMENT.title}</h2>
+        <p>{AppConstants.PrivacyPage.CONTENTS.ADVERTISEMENT.description}</p>
         <p>
           Cookieを無効にする方法やGoogle Adsenseに関する詳細は「
           <a
@@ -33,12 +26,8 @@ const Privacy = () => {
           </a>
           」をご確認ください。
         </p>
-        <h2>アクセス解析ツールについて</h2>
-        <p>
-          当サイトでは、Googleによるアクセス解析ツールであるGoogle Analyticsを利用しています。 Google
-          Analyticsはトラフィックデータの収集のためにCookieを使用しております。
-          トラフィックデータは匿名で収集されており、個人を特定するものではありません。
-        </p>
+        <h2>{AppConstants.PrivacyPage.CONTENTS.ANALYSIS.title}</h2>
+        <p>{AppConstants.PrivacyPage.CONTENTS.ANALYSIS.description}</p>
       </article>
     </>
   );
