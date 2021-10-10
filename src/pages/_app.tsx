5c4eb09e1005549cbd97053a1eb4c23398aacdf2
muta <muttaroni8827@gmail.com>
2021-10-10 16:02:25 +0900

    style: refine code styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,13 +33,13 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GOOGLE_ANALYTICS_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GOOGLE_ANALYTICS_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
             }}
           />
           <Script
