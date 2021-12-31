dc5a475191b7b592cbb8c6d8904d106b007a13e6
muta <muttaroni8827@gmail.com>
2021-12-31 20:15:12 +0900

    fix(date): modify date method considering Date behavior

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,5 +1,5 @@
 export const formatDate = (dateString: string) => {
-  const currentDate = new Date(dateString);
-  const formatString = `${currentDate.getFullYear()}年${currentDate.getMonth()}月${currentDate.getDate()}日`;
+  const currentDate = new Date(dateString.replace(/-/g, '/'));
+  const formatString = `${currentDate.getFullYear()}年${currentDate.getMonth() + 1}月${currentDate.getDate()}日`;
   return formatString;
 };
