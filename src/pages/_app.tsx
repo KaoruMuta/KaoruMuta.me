e2cbf2bdb5a78b65e86e55f2fd098553b0640f17
muta <muttaroni8827@gmail.com>
2022-01-27 21:55:47 +0900

    refactor(analytics): move script tag to _document.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import Script from 'next/script';
 import { useEffect } from 'react';
 import 'tailwindcss/tailwind.css';
 import 'prismjs/themes/prism-okaidia.css';
 import Layout from 'components/Layout';
-import { GOOGLE_ANALYTICS_ID, pageview } from 'lib/gtag';
+import { pageview } from 'lib/gtag';
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter();
@@ -22,29 +21,6 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
 
   return (
     <>
-      {GOOGLE_ANALYTICS_ID && (
-        <>
-          <Script
-            id="gtag-manager"
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-          />
-          <Script
-            id="gtag-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GOOGLE_ANALYTICS_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-            }}
-          />
-        </>
-      )}
       <Layout currentPath={router.asPath}>
         <Component {...pageProps} />
       </Layout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import Script from 'next/script';
+import { GOOGLE_ANALYTICS_ID } from 'lib/gtag';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -11,6 +12,29 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+          {GOOGLE_ANALYTICS_ID && (
+            <>
+              <Script
+                id="gtag-manager"
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+              />
+              <Script
+                id="gtag-script"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GOOGLE_ANALYTICS_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
           <Script
             data-ad-client={`${process.env.GOOGLE_ADSENSE_CLIENT_ID}`}
             async
