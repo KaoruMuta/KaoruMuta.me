172d82cf22f733eadfaa904815ace278b7fd4bb5
Kaoru Muta <muttaroni8827@gmail.com>
2024-12-28 15:37:21 +0900

    fix: remove tag icon when categories are empty

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -6,7 +6,7 @@ const Description = ({ date, categories }: { date: string; categories: string[] 
     <section className="flex flex-col items-start mt-2 xl:flex-row">
       <time className="block flex-1 text-sm">{`${date}に投稿`}</time>
       <section className="flex flex-wrap flex-1 items-center mt-2 xl:justify-end xl:mt-0">
-        <FaTags className="xl:ml-8" />
+        {categories.length > 0 && <FaTags className="xl:ml-8" />}
         {categories.map((category) => {
           return (
             <Link href={`/blog/categories/${category}`} key={category} className="ml-2 text-sm xl:hover:opacity-hover">
