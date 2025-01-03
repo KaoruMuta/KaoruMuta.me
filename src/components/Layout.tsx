7ce536be4b32ce508cf8bd7f7b1c62f2d362f5f3
Kaoru Muta <muttaroni8827@gmail.com>
2025-01-03 16:03:41 +0900

    refactor: replace div to section in layoutContainer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,11 +4,11 @@ import Header from './Header';
 
 const Layout = ({ children }: { children?: ReactNode }) => {
   return (
-    <div id="layoutContainer" className="flex flex-col font-sans mx-4 sm:mx-16 md:mx-56 h-screen">
+    <section id="layoutContainer" className="flex flex-col font-sans mx-4 sm:mx-16 md:mx-56 h-screen">
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
-    </div>
+    </section>
   );
 };
 
