85fadc31e91772765307e6dceab38afb39f11284
Kaoru Muta <muttaroni8827@gmail.com>
2025-01-03 15:07:45 +0900

    refactor: update copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className="flex pb-8">
-      <span className="block flex-1 font-semibold">&copy; 2021 Kaoru Muta.</span>
+      <span className="block flex-1 font-semibold">&copy; 2025 Kaoru Muta.</span>
       <Link href="privacy" className="block justify-end xl:hover:opacity-hover">
         Privacy Policy
       </Link>
