f419b738736abb2041cd59b7e09f96dd33015b31
Kaoru Muta <muttaroni8827@gmail.com>
2026-05-10 16:07:02 +0900

    fix: rename formatDate argument

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,5 +1,5 @@
-export const formatDate = (date: string) => {
-  const currentDate = new Date(date.replace(/-/g, '/'));
+export const formatDate = (dateString: string) => {
+  const currentDate = new Date(dateString.replace(/-/g, '/'));
   const formatString = `${currentDate.getFullYear()}年${currentDate.getMonth() + 1}月${currentDate.getDate()}日`;
   return formatString;
 };
